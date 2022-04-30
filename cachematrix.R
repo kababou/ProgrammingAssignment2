7bb5aee5d4be34ee79a0a3d8e78a7ea67f7a0260
kababou <ababou.kh@gmail.com>
2022-04-30 17:01:22 +0200

    First try at makeCachematrix / Inspired by course example

--- cachematrix.R
+++ cachematrix.R
@@ -1,12 +1,20 @@
 ## Put comments here that give an overall description of what your
 ## functions do
 
-## Write a short comment describing this function
+## Making a matrix
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  inv <- NULL
+  set <- function (y) {
+    x <<- y
+    inv <<- NULL
+  }
+  get <- function() x
+  setinv <- function(solve) inv <<- solve
+  getinv <- function() inv
+  list(set = set, get = get, setinv = setinv, getinv = getinv)
+  
 }
-
 
 ## Write a short comment describing this function
 
